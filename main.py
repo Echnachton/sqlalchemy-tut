313419ca643642fd4abc7fc70b1d69fbfa23ad16
Echnachton <example@example.com>
2026-07-06 20:33:50 +0200

    two types of commits

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+from sqlalchemy import create_engine, text
+
 def main():
-    print("Hello from sqlalchemy-tut!")
+    engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
+    
+    # Commit as you go - manual commits
+    # with engine.connect() as conn:
+    #     conn.execute(text("CREATE TABLE users (id int, name char)"))
+    #     conn.execute(text("INSERT INTO users (id, name) VALUES (:x, :y)"), [{"x": 1, "y": "Ivo"}])
+    #     conn.commit()
+
+    # [Preferred] Begin once - scoped auto commit/rollback
+    with engine.begin() as conn:
+        conn.execute(text("CREATE TABLE users (id int, name char)"))
+        conn.execute(text("INSERT INTO users (id, name) VALUES (:x, :y)"), [{"x": 1, "y": "Ivo"}])
 
 
 if __name__ == "__main__":
